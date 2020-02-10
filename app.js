8254dfba295c4caa7da2aa63007bf0c8278d2a33
docholliday211 <35431728+docholliday211@users.noreply.github.com>
2020-02-09 16:33:10 -0800

    campground and comment basic models and views working and tested. Currently create and show

--- app.js
+++ app.js
@@ -1,37 +1,106 @@
 const express = require('express');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+const Comment = require('./models/comment');
+// const Comment = require('./models/comment');
+const seedDB = require('./seeds');
 
+seedDB();
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-let campgrounds = [
-    {name: 'Salmon Creek', image: 'https://www.outdoorproject.com/sites/default/files/styles/hero_image_desktop/public/features/dscf9483.jpg?itok=Ljgs4oeN'},
-    {name: "Devil's Punchbowl", image: 'https://oregonwild.org/sites/default/files/punchbowlfall.sungchoi.jpg'},
-    {name: 'Windy Creek', image: 'http://www.wildscenicrogue.com/wp-content/uploads/2016/01/DSC_0109.jpg'}
-  ];
-
+// LANDING - route
 app.get('/', (req, res) =>
 res.render('landing'));
 
+//INDEX - show all campgrounds
 app.get('/campgrounds', (req, res) => {
-
-
-  res.render('campgrounds', {campgrounds: campgrounds});
+  // Get all campgrounds from DB
+  Campground.find({}, (err, allCampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('campgrounds/index', {campgrounds: allCampgrounds});
+    }
+  });
 });
 
+//CREATE - add new campground to DB
 app.post('/campgrounds', (req, res) => {
     let name = req.body.name;
     let image = req.body.image;
-    let newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-    //redirect back to campgrounds Page
-    res.redirect('/campgrounds');
+    let desc = req.body.description;
+    let newCampground = {name: name, image: image, description: desc}
+    //create a new campground and save to DB
+    Campground.create(newCampground, (err, newlyCreated) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect('/campgrounds');
+      }
+    });
 });
 
+//NEW - show form to create new campground
 app.get('/campgrounds/new', (req, res) => {
-  res.render('new.ejs');
+  res.render('campgrounds/new.ejs');
 });
+
+//SHOW - individual campgrounds
+app.get('/campgrounds/:id', (req, res) => {
+  //find the campground with given ID
+  Campground.findById(req.params.id).populate('comments').exec(function(err, foundCampground) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(foundCampground)
+      res.render('campgrounds/show', {campground: foundCampground});
+    }
+  });
+});
+
+// =================================
+// COMMENTS ROUTES
+// =================================
+
+app.get('/campgrounds/:id/comments/new', (req, res) => {
+  //find campground by // ID
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('comments/new', {campground: campground});
+    }
+  })
+});
+
+app.post('/campgrounds/:id/comments', (req, res) => {
+  //lookup campground using ID
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+      res.redirect('/campgrounds');
+    } else {
+      Comment.create(req.body.comment, (err, comment) => {
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect('/campgrounds/' + campground._id);
+        }
+      })
+    }
+  })
+})
+
 
 app.listen(port, () => console.log(`YelpCamp app listening on port ${port}!`))
