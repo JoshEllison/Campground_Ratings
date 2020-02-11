d06d001ba6fb86c0fef5f1c1c1bda381a76f159c
docholliday211 <35431728+docholliday211@users.noreply.github.com>
2020-02-10 19:52:51 -0800

    tryig atlas url

--- app.js
+++ app.js
@@ -23,9 +23,12 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect('mongodb://heroku_z7l5cnzr:ProjectPassword1212@ds237723.mlab.com:37723/heroku_z7l5cnzr');
-// mongodb+srv://<username>:<password>@dbcluster-jbbih.mongodb.net/test?retryWrites=true&w=majority
-// const MONGODB_URI = 'mongodb://localhost/yelp_camp' || 'mongodb://heroku_z7l5cnzr:ProjectPassword1212@ds237723.mlab.com:37723/heroku_z7l5cnzr';
+mongoose.connect('mongodb+srv://josh4ellison:ProjectPassword1212@dbcluster-jbbih.mongodb.net/test?retryWrites=true&w=majority');
+
+// mongodb+srv://josh4ellison:ProjectPassword1212@dbcluster-jbbih.mongodb.net/test?retryWrites=true&w=majority
+
+//works but errors on first button click
+// 'mongodb://heroku_z7l5cnzr:ProjectPassword1212@ds237723.mlab.com:37723/heroku_z7l5cnzr';
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', 'mongodb://heroku_z7l5cnzr:ProjectPassword1212@ds237723.mlab.com:37723/heroku_z7l5cnzr'));
